b9381213ec12903c60b33864be7d92efcb0484fc
Yejin <yejin01e@gmail.com>
2024-08-13 11:20:16 +0900

    [level 0] Title: 안전지대, Time: 0.18 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.py
+++ 프로그래머스/0/120866. 안전지대/안전지대.py
@@ -4,9 +4,9 @@ def solution(board):
     
     for i in range(n):
         for j in range(n):
-            if board[i][j] == 0:
+            if board[i][j] == 0:               
                 if (mine(i-1, j-1, n) and board[i-1][j-1] == 1) or (mine(i-1, j, n) and board[i-1][j] == 1) or (mine(i-1, j+1, n) and board[i-1][j+1] == 1) or (mine(i, j-1, n) and board[i][j-1] == 1) or (mine(i, j+1, n) and board[i][j+1] == 1) or (mine(i+1, j-1, n) and board[i+1][j-1] == 1) or (mine(i+1, j, n) and board[i+1][j] == 1) or (mine(i+1, j+1, n) and board[i+1][j+1] == 1):
-                    board[i][j] = 2
+                    board[i][j] = 2    # 본인을 제외한 주변 8칸에 폭탄이 있다면, 본인은 위험지역으로 분류 
     
     for i in range(n):
         for j in range(n):
